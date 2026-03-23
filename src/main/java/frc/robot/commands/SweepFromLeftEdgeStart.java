5b51b6f7f251604d41d8e9dbf684444c7fe0999b
sarahwiens <sarahwiens88@gmail.com>
2026-03-23 17:57:23 -0400

    Update SweepFromLeftEdgeStart.java

--- src/main/java/frc/robot/commands/SweepFromLeftEdgeStart.java
+++ src/main/java/frc/robot/commands/SweepFromLeftEdgeStart.java
@@ -13,84 +13,67 @@ import frc.robot.subsystems.ShooterSubsystem;
 
 public class SweepFromLeftEdgeStart extends SequentialCommandGroup {
 
-    private final DriveSubsystem m_drive;
-    private final IntakeSubsystem m_intake;
-
     public SweepFromLeftEdgeStart(
             DriveSubsystem drive,
             IntakeSubsystem intake,
             ShooterSubsystem shooter) {
         
-        m_drive = drive;
-        m_intake = intake;
-        
         addCommands(
            
-            // Drive Forward + Prapare for intake                                    ~ 4.2 s
+            // Drive Forward + Prapare for intake ~ 3.6 s
             new ParallelDeadlineGroup(
-                drive.driveForwardMeters(2.1),                   // 4.2 sec
-                new WaitCommand(2.8)
+                new SetDriveScaleCommand(
+                    drive,1.0, 1.0),
+                drive.driveForwardMeters(2.1), // 3.6 sec
+                new WaitCommand(2)
                     .andThen(new HoldShootCommand(shooter)
-                    .withTimeout(1.2)),                         // 4.0 sec
-                new WaitCommand(3.5)
-                    .andThen(new IntakeDownCommand(intake))             // 4.0 sec
+                    .withTimeout(1.2)), // 3.2 sec
+                new WaitCommand(2.5)
+                    .andThen(new IntakeDownCommand(intake)) // 3.0 sec
             ),
       
-            // Large Curve Sweep                                                      ~ 3.2 sec
+            // Large Curve Sweep ~ 3.5 sec
             new ParallelDeadlineGroup(
-                new RunIntakeRollerCommand(intake),                     // cont.
-                drive.turnRelative(40),                         // 1.1 sec
-                drive.driveForwardMeters(1.6)                    // 3.2 sec
+                new SetDriveScaleCommand(
+                    drive,0.83, 0.08),
+                new RunIntakeRollerCommand(intake),             
+                drive.turnRelative(40), // 3.5 sec
+                drive.driveForwardMeters(1.6) // 3.5 sec
             ),
 
+            // Small Curve Sweep ~ 3.5 sec
+            new ParallelDeadlineGroup(
+                new SetDriveScaleCommand(
+                    drive,0.75, 0.28),
+                new RunIntakeRollerCommand(intake),             
+                drive.turnRelative(140), // 3.5 sec
+                drive.driveForwardMeters(1.4) // 3.5 sec
+            ),
+
+            // Position to shoot ~ 1.9 sec
+            new ParallelDeadlineGroup(
+                new SetDriveScaleCommand(
+                    drive,0.8, 0.6),
+                new RunIntakeRollerCommand(intake),
+                drive.turnRelative(140) // 1.9 sec
+            ),  
            
-
-            // Move intake to DOWN
-            new IntakeDownCommand(intake),
-   
-    
+            //Shoot ~ 2.0 sec
             new ParallelDeadlineGroup(
-                m_drive.driveForwardMeters(1.5),
-                new RunIntakeRollerCommand(m_intake)
-            ),
-         
+                new IntakeUpCommand(intake),// 1.8 sec
+                new HoldShootCommand(shooter)
+                    .withTimeout(2.0) // 2.0 sec
+            ),  
+
+            //Position for TeleOp ~ 2.5 sec
             new ParallelDeadlineGroup(
-                m_drive.turnRelative(-90),
-                new RunIntakeRollerCommand(m_intake)
-            ),
-
-            new ParallelDeadlineGroup(
-                m_drive.driveForwardMeters(4),
-                new RunIntakeRollerCommand(m_intake)
-            ),
-            new ParallelDeadlineGroup(
-                m_drive.turnRelative(90),
-                new RunIntakeRollerCommand(m_intake)
-            ),
-            
-            //Move intake to UP
-            new IntakeUpCommand(intake),
-
-            // Shoot any balls picked up
-            new HoldShootCommand(shooter).withTimeout(1),
-
-            //Move intake down
-            new IntakeDownCommand(intake),
-
-            // Position for TeleOp while running intake roller
-             new ParallelDeadlineGroup(
-                m_drive.turnRelative(-135),
-                new RunIntakeRollerCommand(m_intake)
-            ),
-              new ParallelDeadlineGroup(
-                m_drive.driveForwardMeters(2.0),
-                new RunIntakeRollerCommand(m_intake)
-            ),
-             new ParallelDeadlineGroup(
-                m_drive.turnRelative(180),
-                new RunIntakeRollerCommand(m_intake)
+                new SetDriveScaleCommand(
+                    drive,0.6, 0.25),
+                drive.driveForwardMeters(-0.6), // 2.5 sec 
+                drive.turnRelative(-55), // 2.5 sec
+                new IntakeTravelCommand(intake) // 1.5 sec
             )
-
+            
         );
     }
 }
